2fa656928e9182a31aa6ceadfce95bc6c73d0cd8
Ambrou <apetitgenet@free.fr>
2014-06-24 19:34:22 +0200

    Mise en place de tests unitaires

--- front/urls.py
+++ front/urls.py
@@ -1,6 +1,11 @@
 #-*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
+from front.views import AboutView
 
-urlpatterns = patterns('front.views',
-    url(r'^creerdepot/$', 'creerDepot'),
-)
+# urlpatterns = patterns('front.views',
+    # url(r'^creerdepot/$', 'creerDepot'),
+# )
+
+urlpatterns = [
+    url(r'^creerdepot/$', AboutView.as_view()),
+]
--- front/views.py
+++ front/views.py
@@ -1,19 +1,42 @@
 #-*- coding: utf-8 -*-
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views.generic import View
 from front.forms import CreerDepotForm
 
-def creerDepot(request):
-	if request.method == 'POST':  # S'il s'agit d'une requête POST
-		form = CreerDepotForm(request.POST)  # Nous reprenons les données
+# def creerDepot(request):
+	# if request.method == 'POST':  # S'il s'agit d'une requête POST
+		# form = CreerDepotForm(request.POST)  # Nous reprenons les données
 
-		if form.is_valid(): # Nous vérifions que les données envoyées sont valides
+		# if form.is_valid(): # Nous vérifions que les données envoyées sont valides
 
-			# Ici nous pouvons traiter les données du formulaire
-			nomDepot = form.cleaned_data['nomDepot']
+			# # Ici nous pouvons traiter les données du formulaire
+			# nomDepot = form.cleaned_data['nomDepot']
 		
 
-	else: # Si ce n'est pas du POST, c'est probablement une requête GET
-		form = CreerDepotForm()  # Nous créons un formulaire vide
+	# else: # Si ce n'est pas du POST, c'est probablement une requête GET
+		# form = CreerDepotForm()  # Nous créons un formulaire vide
 
-	return render(request, 'creerDepot.html', locals())
+	# return render(request, 'creerDepot.html', locals())
+
+
+# Create your views here.
+class AboutView(View):
+	form_class = CreerDepotForm
+	initial = {'key': 'value'}
+	template_name = 'creerDepot.html'
+
+	def get(self, request, *args, **kwargs):
+		# return HttpResponse('Hello, World!')
+		form = self.form_class()
+		# return render(request, self.template_name, {'form': form})
+		return render(request, 'creerDepot.html', locals())
+		
+	def post(self, request, *args, **kwargs):
+		form = self.form_class(request.POST)
+		if form.is_valid():
+			# <process form cleaned data>
+			nomDepot = form.cleaned_data['nomDepot']
+
+		return render(request, self.template_name, {'form': form})
